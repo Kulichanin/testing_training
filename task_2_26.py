a28d73c96f0c2aa1a9d3ed48c487bd6acb22f140
Dmitriy <KulikovDV@protonmail.com>
2023-04-25 15:55:16 +0000

    Solved task_2_26

--- task_2_26.py
+++ task_2_26.py
@@ -7,18 +7,31 @@ def minus(num_1:int, num_2:int) -> int:
 def multiply(num_1:int, num_2:int) -> int:
     return num_1 * num_2
 
-def divide(num_1:int, num_2:int) -> float:
-    return num_1 / num_2
+def divide(num_1:int, num_2:int):
+    try:
+        return int(num_1 / num_2)
+    except ZeroDivisionError:
+        print('На ноль делить нельзя!')
 
 def main():
-    #TODO: Подумать над выводом операции не через логику а через словарь
-
     try:
-        num_1 = int(input('Напишите первое число: '))
-        sign = int(input('Введите цифру от 1 до 4 где: 1 = +, 2 = -, 3 = *, 4 = : '))
-        num_2 = int(input('Напишите второе число: '))
+        num_one = int(input('Напишите первое число: '))
+        sign = input('Введите арифметический знак +, -, *, /: ')
+        num_two = int(input('Напишите второе число: '))
     except ValueError:
         print('Ввидите целое число')
+        exit()
+
+    log_dict = {
+            '+' : plus(num_one, num_two),
+            '-' : minus(num_one, num_two),
+            '*' : multiply(num_one, num_two),
+            '/' : divide(num_one, num_two)}
+
+    try:
+        print(log_dict[sign])
+    except KeyError:
+        print('Некорректный знак!')
 
 
 if __name__ == "__main__":
