7823e4f23b2d1f634a8f434d44b0b216d49bf533
Dmitriy <KulikovDV@protonmail.com>
2023-04-26 18:01:52 +0000

    Fixed bug with child class and method output

--- task_3_6.py
+++ task_3_6.py
@@ -8,14 +8,7 @@ class Car():
                  price,
                  mileage 
                  ):
-        """
-            self.model: название модели
-            self.year_of_release: год выпуска
-            self.engine_capacity: объем двигателя
-            self.price: стоимость
-            self.mileage: пробег
-            self.num_of_wheels: количество колес. По умолчанию 4 
-        """
+
         self.model = model
         self.year_of_release = year_of_release
         self.engine_capacity = engine_capacity
@@ -24,12 +17,16 @@ class Car():
         self.num_of_wheels = 4
 
     def description_car(self):
-        """Получение описания человека"""
-        description = (f'{self.model} {self.year_of_release} {self.engine_capacity} {self.price} {self.mileage} {self.num_of_wheels}')
+        """Получение описания автомобиля"""
+        description = (f'Название модели: {self.model}.\n' 
+                       f'Год производства: {self.year_of_release}.\n' 
+                       f'Объем двигателя: {self.engine_capacity}.\n'
+                       f'Стоимость: {self.price}.\n' 
+                       f'Пробег: {self.mileage}.\n' 
+                       f'Кол-во колеc: {self.num_of_wheels}.\n')
         print(description)
 
-
-class Truck():
+class Truck(Car):
     """Модель грузовика"""
 
     def __init__(self, model, year_of_release, 
@@ -37,4 +34,4 @@ class Truck():
         """Инициализация атрибутов класса-родителя"""
         super().__init__(model, year_of_release, 
                          engine_capacity, price, mileage)
-
+        self.num_of_wheels = 8
