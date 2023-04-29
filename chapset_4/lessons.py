d24d964c1ee43e129d653aeb2d597d63c0e971bd
Dmitriy <KulikovDV@protonmail.me>
2023-04-29 18:08:49 +0300

    create login seleniium

--- chapset_4/lessons.py
+++ chapset_4/lessons.py
@@ -1,7 +1,14 @@
 from time import sleep
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome(executable_path='./driver/chromedriver')
 driver.get('https://www.saucedemo.com/')
+username = driver.find_element(By.XPATH, '//input[@data-test="username"]')
+username.send_keys('standard_user')
+password = driver.find_element(By.XPATH, '//*[@id="password"]')
+password.send_keys('secret_sauce')
+author = driver.find_element(By.XPATH, '//*[@id="login-button"]')
+author.click()
 sleep(5)
 driver.close()
