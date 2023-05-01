c972bff53be2cde6e46ad9e853b99673c616df6a
Dmitriy <KulikovDV@protonmail.me>
2023-05-01 09:49:21 +0300

    fix test_1. Crete metod browers won`t close

--- chapset_4/test_1.py
+++ chapset_4/test_1.py
@@ -1,7 +1,15 @@
 from time import sleep
 from selenium import webdriver
-from selenium.webdriver.common.by import By
 from datetime import datetime
+
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+
+option = Options()
+option.add_experimental_option('detach', True)
+
+driver = webdriver.Chrome(executable_path='./driver/chromedriver', options=option)
+
 
 driver = webdriver.Chrome(executable_path='./driver/chromedriver')
 driver.get('https://www.saucedemo.com/')
@@ -13,9 +21,9 @@ password.send_keys('secret_sauce')
 print('input pass')
 author = driver.find_element(By.XPATH, '//input[@id="login-button"]')
 author.click()
-
+sleep(1)
 now_date = datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
 name_screen = 'screenshot_' + now_date + '.png'
 driver.save_screenshot('./screen/' + name_screen)
-
-driver.close()
+sleep(1)
+#:driver.close()
