56da1ccc85d113ae361ecd31a85137bb89c16f8b
Dmitriy <KulikovDV@protonmail.me>
2023-05-09 09:46:40 +0300

    solved task 4 27

--- chapset_4/task_4_27.py
+++ chapset_4/task_4_27.py
@@ -2,68 +2,26 @@
 4.27 Запросить у пользователя номер товара,
 а затем отработать всю бизнес логику магазина.
 """
-
-#!TODO Попробовать сделать через ООП. 
-# Каждый товар в магазине это как новый обьект который мы создаем через ввод
-"""
-print('Идентификатор имени товара 1 Sauce Labs Backpack 
-print('Идентификатор имени товара 2 Sauce Labs Bike Light 
-print('Идентификатор имени товара 3 Sauce Labs Bolt T-Shirt 
-print('Идентификатор имени товара 4 Sauce Labs Fleece Jacket 
-print('Идентификатор имени товара 5 Sauce Labs Onesie 
-print('Идентификатор имени товара 6 Test.allTheThings() T-Shirt (Red) 
-"""
-
-"""
-Формурование ссылки htnl элемнета для выделения и взаимодействия
-//*[@id="inventory_container"]/div/div[1-6]/div[2]/ + 
-    + div[1]/a - имя товара
-    + div[2]/div - цена товара
-    + div[2]/button - кнопка добавления в корзину
-
-"""
-"""
-Добавление в корзину
-'//*[@class="shopping_cart_container"]/a'
-"""
-"""
-print('Информация о товаре в корзине
-'//*[@class="inventory_item_name"]' - имя товара
-'//*[@class="inventory_item_price"]' - price
-"""
-"""
-данные и проход в оверьб забрать из 4_17
-"""
-
-"""
-подтверждение товара
-'//*[@class="inventory_item_name"]' - имя товара
-'//*[@class="inventory_item_price"]' - price
-'//*[@class="summary_subtotal_label"]' - price total
-"""
 from selenium import webdriver
 from datetime import datetime
+from time import sleep
 
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys    
+from selenium.webdriver.common.keys import Keys 
+
+
 def screenshot() -> str:
     """
     Create screenshot
     return: name screenshot
     """
-    driver = webdriver.Chrome(executable_path='./driver/chromedriver')
-
     now_date = datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
     name_screen = 'screenshot_' + now_date + '.png'
     driver.save_screenshot('./screen/' + name_screen)
     return name_screen
 
 def main(number:int) -> None:
-    option = Options()
-    option.add_experimental_option('detach', True) 
-    driver = webdriver.Chrome(executable_path='./driver/chromedriver', 
-                              options=option)
     # Authorication in site
     driver.get('https://www.saucedemo.com/')
     driver.implicitly_wait(1)
@@ -73,32 +31,76 @@ def main(number:int) -> None:
     password = driver.find_element(By.XPATH, '//input[@id="password"]')
     password.send_keys('secret_sauce')
     password.send_keys(Keys.ENTER)
-    print('Authorization successful')
-    print(screenshot())
-    
+    print('Авторизация успешна.')
+   
     # Adding the selected product to the cart
-    product_name = driver.find_element(By.XPATH,f'//*[@id="inventory_container"]/div/div[{number}]/div[2]/div/a').text
-    product_price = driver.find_element(By.XPATH,f'//*[@id="inventory_container"]/div/div[{number}]/div[2]/div[2]/div').text
+    product_name = driver.find_element(By.XPATH,
+                                       f'//*[@id="inventory_container"]/div/div[{number}]/div[2]/div/a').text
+    product_price = driver.find_element(By.XPATH,
+                                        f'//*[@id="inventory_container"]/div/div[{number}]/div[2]/div[2]/div').text
     print(f'Вы выбрали товар: {product_name}. Со стоимостью: {product_price}')
-    driver.find_element(By.XPATH,f'//*[@id="inventory_container"]/div/div[{number}]/div[2]/div[2]/button').click()
+    driver.find_element(By.XPATH,
+                        f'//*[@id="inventory_container"]/div/div[{number}]/div[2]/div[2]/button').click()
     print('Товар успешно добавлен в корзину!')
-    #!TODO Все ок но надо поправить рабты функции со скриншотом
-    """
-    #Go to the shopping cart
-    driver.find_element(By.XPATH, '//*[@class="shopping_cart_container"]/a').click()
-    driver
+    sleep(1)
+    print(screenshot())
 
-    #Info product in your cart
-    value_your_cart_product_1 = driver.find_element(By.XPATH, '//*[@id="item_5_title_link"]/div').text
-    value_your_cart_product_2 = driver.find_element(By.XPATH, '//*[@id="item_3_title_link"]/div').text
-    value_price_your_cart_product_1 = driver.find_element(
-            By.XPATH, 
-            '//*[@id="cart_contents_container"]/div/div[1]/div[3]/div[2]/div[2]/div').text
-    value_price_your_cart_product_2 = driver.find_element(
-            By.XPATH, 
-            '//*[@id="cart_contents_container"]/div/div[1]/div[4]/div[2]/div[2]/div').text
-    print(value_your_cart_product_1, value_your_cart_product_2, value_price_your_cart_product_1, value_price_your_cart_product_2)
-    """
+    # Go to the shopping cart
+    driver.find_element(By.XPATH, 
+                        '//*[@class="shopping_cart_container"]/a').click()
+
+    # Save info product in your cart
+    product_name_in_cart = driver.find_element(By.XPATH, '//*[@class="inventory_item_name"]').text
+    product_price_in_cart = driver.find_element(By.XPATH, '//*[@class="inventory_item_price"]').text
+
+    print(f'Информация о продукте в корзине: '
+          f'Имя товара: {product_name_in_cart}. '
+          f'Стоимость: {product_price_in_cart}.')
+    sleep(1)
+    print(screenshot())
+
+    # Go to the checkout user information
+    driver.find_element(By.XPATH, '//*[@id="checkout"]').click()
+    
+    # Input information about user
+    driver.find_element(By.XPATH, '//*[@id="first-name"]').send_keys('Ivan')
+    driver.find_element(By.XPATH, '//*[@id="last-name"]').send_keys('Belekov')
+    driver.find_element(By.XPATH, '//*[@id="postal-code"]').send_keys('123456')
+    
+    # Go to the checkout:overwiew
+    driver.find_element(By.XPATH, '//*[@id="continue"]').click()
+    
+    # Getting data about the name and cost of the goods
+    product_name_in_overwiew = driver.find_element(By.XPATH, 
+                                                   '//*[@class="inventory_item_name"]').text
+    product_price_in_overwiew = driver.find_element(By.XPATH, 
+                                                    '//*[@class="inventory_item_price"]').text
+    product_price_total_text = driver.find_element(By.XPATH, 
+                                                    '//*[@class="summary_subtotal_label"]' ).text
+    
+    product_price_total = product_price_total_text.split('$')
+
+    print(f'Информация о продукте на странице подтверждения товара: ' 
+          f'Имя товара: {product_name_in_overwiew}. ' 
+          f'Стоимость: {product_price_in_overwiew}.')
+    sleep(1)
+    print(screenshot())
+
+    # Checking the name of the cost of goods from all stages
+    assert product_name == product_name_in_overwiew == product_name_in_cart
+    print('Тест имени товара успешный!')
+    assert float(product_price[1:]) == float(product_price_in_cart[1:]) == float(product_price_in_overwiew[1:])  == float(product_price_total[1])
+    print('Тест стоимости товара успешный!')
+
+    # Final page
+    driver.find_element(By.XPATH, '//*[@id="finish"]').click()
+    print(f'Поздравялем! Вы успешно приобрели товар: ' 
+          f'Имя товара: {product_name_in_overwiew}. ' 
+          f'Стоимость: {product_price_in_overwiew}.')
+    sleep(1)
+    print(screenshot())
+    print('Все скриншоты вашего оформления находятся в каталоге: ./screen/')
+    driver.close()
 
 
 if __name__ == "__main__":
@@ -111,4 +113,10 @@ if __name__ == "__main__":
     print('Идентификатор имени товара 5 - Sauce Labs Onesie') 
     print('Идентификатор имени товара 6 - Test.allTheThings() T-Shirt (Red)')
     number = int(input('Номер товара:'))
+    
+    option = Options()
+    option.add_experimental_option('detach', True) 
+    driver = webdriver.Chrome(executable_path='./driver/chromedriver', 
+                              options=option)
+
     main(number)
