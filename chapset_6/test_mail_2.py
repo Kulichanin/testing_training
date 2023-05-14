9f603b84644497fb70555dd4cecc69d1f270ba7c
Dmitriy <KulikovDV@protonmail.me>
2023-05-14 09:57:05 +0300

    solved task 6.1 -6.3

--- chapset_6/test_mail_2.py
+++ chapset_6/test_mail_2.py
@@ -3,11 +3,9 @@ import pytest
 @pytest.fixture()
 def set_up():
     print('Вход в систему выполнен')
-    yield
-    print('Выход из системы')
 
-def test_sending_mail_1(set_up):
+def test_sending_mail_1():
     print('Письмо отправлено')
 
-def test_sending_mail_2(set_up):
+def test_sending_mail_2():
     print('Письмо отправлено')
