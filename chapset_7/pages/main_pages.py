5319df82a3fa60ad9cf3663f1e649b66d44aed76
Dmitriy <KulikovDV@protonmail.me>
2023-05-18 21:15:37 +0300

    fix main_page and create cart_page. update test

--- chapset_7/pages/main_pages.py
+++ chapset_7/pages/main_pages.py
@@ -11,6 +11,7 @@ class Main_page(Base):
         super().__init__(driver)
         self.driver = driver
         self.product_name = f'//*[@id="inventory_container"]/div/div[{num_product}]/div[2]/div/a'
+        self.product_price = f'//*[@id="inventory_container"]/div/div[{num_product}]/div[2]/div[2]/div'
         self.product = f'//*[@id="inventory_container"]/div/div[{num_product}]/div[2]/div[2]/button'
         self.cart = '//*[@class="shopping_cart_container"]/a'
 
@@ -24,6 +25,15 @@ class Main_page(Base):
     def get_product_name(self):
         return WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.XPATH, self.product_name)))
 
+    def get_product_price(self):
+        return WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.XPATH, self.product_price)))
+    
+    def text_product_name(self):
+        return self.get_product_name().text
+    
+    def price_product(self):
+        return self.get_product_price().text
+
     def click_select_product(self):
         self.get_product().click()
         print('Click product')
@@ -32,10 +42,10 @@ class Main_page(Base):
         self.get_cart().click()
         print('Click cart')
 
-
-
-    def select_product(self) -> None:
+    def select_product(self):
         self.get_current_page()
-        name_product = self.click_select_product()
+        self.click_select_product()
+        product_name = self.text_product_name()
+        product_price = self.price_product()
         self.click_cart()
-        return name_product
+        return product_name, product_price
--- chapset_7/test/test_by_product.py
+++ chapset_7/test/test_by_product.py
@@ -7,6 +7,7 @@ from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 from pages.login_pages import Login_page
 from pages.main_pages import Main_page
+from pages.cart_page import Cart_page
 
 def test_select_product(number:int = 3):
     # Create driver from webdriver_manager.chrome 
@@ -17,27 +18,19 @@ def test_select_product(number:int = 3):
     login.authorization()
     
     mp = Main_page(driver, number)
-    product_name = mp.select_product()
-
-    # Adding the selected product to the cart
-    #product_name = (WebDriverWait(driver, 30).until(EC.element_to_be_clickable((By.XPATH,
-    #                                   f'//*[@id="inventory_container"]/div/div[{number}]/div[2]/div/a')))).text
-    product_price = (WebDriverWait(driver, 30).until(EC.element_to_be_clickable((By.XPATH,
-                                        f'//*[@id="inventory_container"]/div/div[{number}]/div[2]/div[2]/div')))).text
+    product_name, product_price = mp.select_product()
+    
     print(f'Вы выбрали товар: {product_name}. Со стоимостью: {product_price}')
-    #(WebDriverWait(driver, 30).until(EC.element_to_be_clickable((By.XPATH,
-    #                    f'//*[@id="inventory_container"]/div/div[{number}]/div[2]/div[2]/button')))).click()
     print('Товар успешно добавлен в корзину!')
     sleep(1)
-    
 
-    # Go to the shopping cart
-    (WebDriverWait(driver, 30).until(EC.element_to_be_clickable((By.XPATH, 
-                        '//*[@class="shopping_cart_container"]/a')))).click()
-
+    cp = Cart_page(driver)
+    cp.product_conformation()
+    sleep(3)
+    """ 
     # Save info product in your cart
-    product_name_in_cart = (WebDriverWait(driver, 30).until(EC.element_to_be_clickable((By.XPATH, '//*[@class="inventory_item_name"]')))).text
-    product_price_in_cart = (WebDriverWait(driver, 30).until(EC.element_to_be_clickable((By.XPATH, '//*[@class="inventory_item_price"]')))).text
+    product_name_in_cart = (WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.XPATH, '//*[@class="inventory_item_name"]')))).text
+    product_price_in_cart = (WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.XPATH, '//*[@class="inventory_item_price"]')))).text
 
     print(f'Информация о продукте в корзине: '
           f'Имя товара: {product_name_in_cart}. '
@@ -46,22 +39,22 @@ def test_select_product(number:int = 3):
     
 
     # Go to the checkout user information
-    (WebDriverWait(driver, 30).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="checkout"]')))).click()
-                                                                
+    (WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="checkout"]')))).click()
+    """                                                            
     # Input information about user
-    (WebDriverWait(driver, 30).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="first-name"]')))).send_keys('Ivan')
-    (WebDriverWait(driver, 30).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="last-name"]')))).send_keys('Belekov')
-    (WebDriverWait(driver, 30).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="postal-code"]')))).send_keys('123456')
+    (WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="first-name"]')))).send_keys('Ivan')
+    (WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="last-name"]')))).send_keys('Belekov')
+    (WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="postal-code"]')))).send_keys('123456')
     
     # Go to the checkout:overwiew
-    (WebDriverWait(driver, 30).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="continue"]')))).click()
+    (WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="continue"]')))).click()
     
     # Getting data about the name and cost of the goods
-    product_name_in_overwiew = (WebDriverWait(driver, 30).until(EC.element_to_be_clickable((By.XPATH, 
+    product_name_in_overwiew = (WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.XPATH, 
                                                    '//*[@class="inventory_item_name"]')))).text
-    product_price_in_overwiew = (WebDriverWait(driver, 30).until(EC.element_to_be_clickable((By.XPATH, 
+    product_price_in_overwiew = (WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.XPATH, 
                                                     '//*[@class="inventory_item_price"]')))).text
-    product_price_total_text = (WebDriverWait(driver, 30).until(EC.element_to_be_clickable((By.XPATH, 
+    product_price_total_text = (WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.XPATH, 
                                                     '//*[@class="summary_subtotal_label"]' )))).text
     
     product_price_total = product_price_total_text.split('$')
@@ -73,22 +66,25 @@ def test_select_product(number:int = 3):
     
 
     # Checking the name of the cost of goods from all stages
-    assert product_name == product_name_in_overwiew == product_name_in_cart
+    #assert product_name == product_name_in_overwiew == product_name_in_cart
+    assert product_name == product_name_in_overwiew
+
     print('Тест имени товара успешный!')
-    assert float(product_price[1:]) == float(product_price_in_cart[1:]) == float(product_price_in_overwiew[1:])  == float(product_price_total[1])
+    #assert float(product_price[1:]) == float(product_price_in_cart[1:]) == float(product_price_in_overwiew[1:])  == float(product_price_total[1])
+    assert float(product_price[1:])  == float(product_price_in_overwiew[1:])  == float(product_price_total[1])
     print('Тест стоимости товара успешный!')
 
     # Final page
-    (WebDriverWait(driver, 30).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="finish"]')))).click()
+    (WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="finish"]')))).click()
     print(f'Поздравялем! Вы успешно приобрели товар: ' 
           f'Имя товара: {product_name_in_overwiew}. ' 
           f'Стоимость: {product_price_in_overwiew}.')
     sleep(1)
 
+
 def main():
     num = input('Введите номер товара: ')
     test_select_product(int(num))
-    print('test start')
 
 if __name__ == '__main__':
     main()
