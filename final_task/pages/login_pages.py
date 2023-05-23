8dc50aead129e39ebbfa792ddacad149bc2d2b5b
Dmitriy <KulikovDV@protonmail.me>
2023-05-23 21:29:18 +0300

    create login page

--- final_task/pages/login_pages.py
+++ final_task/pages/login_pages.py
@@ -11,10 +11,11 @@ class Login_page(Base):
         super().__init__(driver)
         self.driver = driver
         
-        self.url = 'https://www.saucedemo.com/'
-        self.user_name = '//input[@data-test="username"]'
-        self.password = '//input[@id="password"]'
-        self.login_button = '//input[@id="login-button"]'
+        self.url = 'https://www.citilink.ru/'
+        self.user_name = '//input[@name="login"]'
+        self.password = '//input[@name="pass"]'
+        self.login_button = '//div[@data-meta-name="UserButtonContainer"]'
+        self.login_button_enter = '//button[@class="e4uhfkv0 css-1yh1imp e4mggex0"]'
         self.main_word = '//span[@class="title"]'
 
 
@@ -29,6 +30,10 @@ class Login_page(Base):
     def get_login_button(self):
         return WebDriverWait(self.driver, 3).until(
                 EC.element_to_be_clickable((By.XPATH, self.login_button)))
+    
+    def get_login_button_enter(self):
+        return WebDriverWait(self.driver, 3).until(
+                EC.element_to_be_clickable((By.XPATH, self.login_button_enter)))
 
     def get_main_word(self):
         return WebDriverWait(self.driver, 3).until(
@@ -46,37 +51,16 @@ class Login_page(Base):
         self.get_login_button().click()
         print('Click login button')
 
+    def click_login_button_enter(self):
+        self.get_login_button_enter().click()
+        print('Click login button enter')
+
 
 
     def authorization(self) -> None:
         self.driver.get(self.url)
         self.get_current_page()
-        self.input_username('standard_user')
-        self.input_password('secret_sauce')
         self.click_login_button()
-
-        # Проверка доступа к странице с продуктами. Ожидание элемента Product 10 секунд.
-        # В случае неуспешной авторизации в работу вступает обработчик ошибок.
-        try:
-            check_string = self.assert_word(self.get_main_word(), 'Products')
-            return check_string
-        except TimeoutException:
-            # Поиcк ошибки и ее вывод к консоль
-            check_string_error = WebDriverWait(self.driver, 5).until(EC.element_to_be_clickable((By.XPATH,  '//h3[@data-test="error"]')))
-            print(f'User authorization failed! {check_string_error.text}')
-     
-    def authorization_list(self, user_login_list:list[str], user_pass:str) -> None:
-        for i in user_login_list:
-            result = self.authorization()
-            if result:
-                #Если авторизация нормальная, то выходим с помощью log out
-                (WebDriverWait(self.driver, 3).until(
-                    EC.element_to_be_clickable((By.XPATH,'//button[@id="react-burger-menu-btn"]')))).click()
-
-                (WebDriverWait(self.driver, 3).until(
-                    EC.element_to_be_clickable((By.XPATH,'//a[@id="logout_sidebar_link"]')))).click()
-
-            else:
-                # Если с авторизацией проблемы, то перезагружаем страницу.
-                # Таким образом логин и пароль очищаются.
-                self.driver.refresh()
+        self.input_username('mr.gn0m@yandex.ru')
+        self.input_password('1235789460')
+        self.click_login_button_enter()
--- final_task/test/test_by_product.py
+++ final_task/test/test_by_product.py
@@ -9,52 +9,20 @@ from pages.client_inf_product import Client_information_pages
 from pages.payment_page import Payment_page
 from pages.finish_page import Finish_page
 
-def test_select_product(number:int = 3):
+def test_select_product():
     # Create driver from webdriver_manager.chrome 
     driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
-
+    driver.fullscreen_window()
+    #!TODO FULL SCREEN PROBABLY
     # Authorication in site
     login = Login_page(driver)
     login.authorization()
-    
-    mp = Main_page(driver, number)
-    product_name, product_price = mp.select_product()
-    
-    print(f'Вы выбрали товар: {product_name}. Со стоимостью: {product_price}')
-    print('Товар успешно добавлен в корзину!')
-    sleep(1)
-
-    cp = Cart_page(driver)
-    cp.product_conformation()
-    sleep(1)
-    
-    cli = Client_information_pages(driver)
-    cli.input_information()
-    sleep(1)
-
-    # Getting data about the name and cost of the goods
-    sleep(1)
-    pay_page = Payment_page(driver)
-    product_name_in_overwiew, product_price_in_overwiew, product_price_total = pay_page.payment()
-
-    # Checking the name of the cost of goods from all stages
-    assert product_name == product_name_in_overwiew
-    print('Тест имени товара успешный!')
-    assert float(product_price[1:])  == float(product_price_in_overwiew[1:])  == float(product_price_total[1])
-    print('Тест стоимости товара успешный!')
-
-    # Final page
-    print(f'Поздравялем! Вы успешно приобрели товар: ' 
-          f'Имя товара: {product_name_in_overwiew}. ' 
-          f'Стоимость: {product_price_in_overwiew}.')
-    fin = Finish_page(driver)
-    fin.finish()
-    sleep(1)
+    print('YES!')
+    sleep(3)
 
 
 def main():
-    num = input('Введите номер товара: ')
-    test_select_product(int(num))
+    test_select_product()
 
 if __name__ == '__main__':
     main()
