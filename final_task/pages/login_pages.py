91433474bc78db3b5ac93a4e3bdb080bf48df292
Dmitriy <KulikovDV@protonmail.me>
2023-05-24 18:54:20 +0300

    change input pass and login

--- final_task/pages/login_pages.py
+++ final_task/pages/login_pages.py
@@ -10,33 +10,33 @@ class Login_page(Base):
         super().__init__(driver)
         self.driver = driver
         
-        self.url = 'https://www.citilink.ru/'
-        self.user_name = '//input[@name="login"]'
-        self.password = '//input[@name="pass"]'
-        self.login_button = '//div[@data-meta-name="UserButtonContainer"]'
-        self.login_button_enter = '//button[@class="e4uhfkv0 css-1yh1imp e4mggex0"]'
-        self.main_word = '//span[@class="title"]'
+        self._url = 'https://www.citilink.ru/'
+        self._user_name = '//input[@name="login"]'
+        self._password = '//input[@name="pass"]'
+        self._login_button = '//div[@data-meta-name="UserButtonContainer"]'
+        self._login_button_enter = '//button[@class="e4uhfkv0 css-1yh1imp e4mggex0"]'
+        self._main_word = '//span[@class="title"]'
 
 
     def get_user_name(self):
         return WebDriverWait(self.driver, 3).until(
-                EC.element_to_be_clickable((By.XPATH, self.user_name)))
+                EC.element_to_be_clickable((By.XPATH, self._user_name)))
 
     def get_password(self):
         return WebDriverWait(self.driver, 3).until(
-                EC.element_to_be_clickable((By.XPATH,  self.password)))
+                EC.element_to_be_clickable((By.XPATH,  self._password)))
 
     def get_login_button(self):
         return WebDriverWait(self.driver, 3).until(
-                EC.element_to_be_clickable((By.XPATH, self.login_button)))
+                EC.element_to_be_clickable((By.XPATH, self._login_button)))
     
     def get_login_button_enter(self):
         return WebDriverWait(self.driver, 3).until(
-                EC.element_to_be_clickable((By.XPATH, self.login_button_enter)))
+                EC.element_to_be_clickable((By.XPATH, self._login_button_enter)))
 
     def get_main_word(self):
         return WebDriverWait(self.driver, 3).until(
-                EC.element_to_be_clickable((By.XPATH, self.main_word)))
+                EC.element_to_be_clickable((By.XPATH, self._main_word)))
 
     def input_username(self, username):
         self.get_user_name().send_keys(username)
@@ -56,11 +56,11 @@ class Login_page(Base):
 
 
 
-    def authorization(self) -> None:
-        self.driver.get(self.url)
+    def authorization(self, username, password) -> None:
+        self.driver.get(self._url)
         self.get_current_page()
         self.driver.maximize_window()
         self.click_login_button()
-        self.input_username('mr.gn0m@yandex.ru')
-        self.input_password('1')
+        self.input_username(username)
+        self.input_password(password)
         self.click_login_button_enter()
--- final_task/test/test_by_product.py
+++ final_task/test/test_by_product.py
@@ -8,14 +8,13 @@ from pages.main_pages import Main_page
 from pages.cart_page import Cart_page
 from pages.client_inf_product import Client_information_pages
 from pages.payment_page import Payment_page
-from pages.finish_page import Finish_page
 
 def test_select_product():
     # Create driver from webdriver_manager.chrome
     driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
     # Authorication in site
     login = Login_page(driver)
-    login.authorization()
+    login.authorization(username, password)
     print('YES!')
     sleep(3)
 
@@ -24,4 +23,6 @@ def main():
     test_select_product()
 
 if __name__ == '__main__':
+    username = input('input username: ')
+    password = input('input password: ')
     main()
